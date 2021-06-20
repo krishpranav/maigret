cdc9372484778c54e5aeaf37e75c3c05377b8c7a
krishpranav <krisna.pranav@gmail.com>
2021-06-20 11:22:54 +0530

    [+] generate sites from md [+]

--- generate_sites_md.py
+++ generate_sites_md.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
-# generate sites from md 
+# this file contains function for finding the websites from the sites.md
+
 
 # imports
 from typing import Dict
 import json
 
 site_dict: Dict[str, Dict] = json.loads(open("data.json", "r", encoding="utf-8").read())
+
+with open("sites.md", "w", encoding="utf-8") as file:
+    file.write(f"# {len(site_dict)} sites are supported!\n")
+    for site in sorted(site_dict.keys()):
+        file.write(f" - [{site}]({site_dict[site]['urlMain']})\n")
+    file.write(
+        "# Removed sites\nPlease refer [here](https://github.com/sherlock-project/sherlock/blob/master/removed_sites.md)"
+    )
