4c38f77450fc0b449e157e8742ebe581080bcf50
krishpranav <krisna.pranav@gmail.com>
2021-06-20 11:47:51 +0530

    [+] forwarding proxy request [+]

--- chrome/proxy.go
+++ chrome/proxy.go
@@ -1,3 +1,16 @@
 package chrome
 
+import (
+	"net"
+	"net/http/httputil"
+	"net/url"
+)
+
 const listeningURL string = "127.0.0.1"
+
+type forwardingProxy struct {
+	targetURL *url.URL
+	server    *httputil.ReverseProxy
+	listener  net.Listener
+	port      int
+}
