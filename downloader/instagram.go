d0b7a3cafe3bb25aba17fce25486ff23cd66e457
krishpranav <krisna.pranav@gmail.com>
2021-06-20 11:56:00 +0530

    [+] out folder [+]

--- downloader/instagram.go
+++ downloader/instagram.go
@@ -2,6 +2,7 @@ package downloader
 
 import (
 	"log"
+	"os"
 	"strings"
 )
 
@@ -9,4 +10,8 @@ func downloadInstagram(url string, logger *log.Logger) {
 
 	_splitURL := strings.Split(url, "/")
 	username := _splitURL[len(_splitURL)-1]
+
+	OUT := "./downloads/" + username + "/instagram/"
+	os.MkdirAll(OUT, os.ModePerm)
+
 }
